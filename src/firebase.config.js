462c42e93a7b9b576b4cd116fe2e8f275684e65e
rafee1100 <rafee.abdullah1@gmail.com>
2021-03-20 22:05:09 +0600

    major changes

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDXiacLQhmY3M1O46myRI-_n1e6hP31_uE",
+  authDomain: "cholo-jai-simple.firebaseapp.com",
+  projectId: "cholo-jai-simple",
+  storageBucket: "cholo-jai-simple.appspot.com",
+  messagingSenderId: "438895733903",
+  appId: "1:438895733903:web:4301dd47c3351964e714fa"
+};
